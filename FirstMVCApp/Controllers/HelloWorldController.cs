59397c32ff0e71da1282d8f4bd07a87482a4b63a
RajeswariGudiwada <rajeswari.gudiwada@sailssoftware.com>
2023-06-12 10:47:53 +0530

    Add project files.

---
+++ FirstMVCApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FirstMVCApp.Models;
+
+namespace FirstMVCApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        private static List<Studentmodel> students = new List<Studentmodel>();
+        public IActionResult Index()
+        {
+           
+            return View(students);
+
+        }
+        public IActionResult Register()
+        {
+            var sdtVM = new Studentmodel();
+            return View(sdtVM);
+
+        }
+        public IActionResult registation(Studentmodel studentmodel)
+        {
+
+            students.Add(studentmodel);
+            return RedirectToAction(nameof(Index));
+
+        }
+
+
+        public IActionResult Hello()
+        {
+            return View();
+        }
+    }
+}
---
+++ FirstMVCApp/Models/Studentmodel.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+
+namespace FirstMVCApp.Models
+{
+    public class Studentmodel
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }   
+    }
+}
